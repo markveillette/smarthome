afc8256a300d679bf4b0996a49aff1d707c939d6
mark.veillette <mark4483@gmail.com>
2017-02-05 11:56:15 -0500

    Added comment to wifi checker

--- wifi_check.sh
+++ wifi_check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# This continuously checks if wifi is connection to local network, and trys to restart
+# if not.
+# Code taken from https://www.raspberrypi.org/forums/viewtopic.php?t=16054
 while true ; do
    if ifconfig wlan0 | grep -q "inet addr:" ; then
       sleep 60
