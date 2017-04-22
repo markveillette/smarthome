30753559c56784e88d0a0e1b9d61cc0d66dc851c
Mark <mark4483@gmail.com>
2017-04-22 17:07:13 -0400

    slight refactoring of ir_remote.  Updated Readme

---
+++ README.md
@@ -0,0 +1,40 @@
+A set of python scripts I use for home automation with my Amazon Echo.
+
+To control these devices, I followed the method described here:
+http://www.makermusings.com/2015/07/13/amazon-echo-and-home-automation/
+
+Included here are scripts for
+
+--  A TV remote controlled by my raspberry pi using lirc
+--  Music streamer on Ubuntu using pianobar
+
+To design the IR LED circut, I followed
+http://randomtutor.blogspot.gr/2013/01/web-based-ir-remote-on-raspberry-pi.html
+
+#####
+## Setting up LIRC for your remote
+#####
+
+1.  Follow tutorial at http://alexba.in/blog/2013/01/06/setting-up-lirc-on-the-raspberrypi/
+
+2. Grab .conf file for your remote at http://lirc-remotes.sourceforge.net/remotes-table.html
+
+3. Copy contents of this file to /etc/lirc/lircd.conf
+
+4. In python script(s), make sure the correct name of the remote is passed to ir_remoste handlers. 
+
+#####
+## Setting up pianobar
+#####
+
+This is a bit ugly, but it works. pianobar.py sshes into a linux machine with pianobar installed on local network and runs a script called run_pianobar.  In my case, this computer is named 'quinn'.  This script that runs on the streaming machine contains the following:
+    #!/bin/bash
+    pianobar &
+    sleep 3600 
+In order for this to work, ssh keys but be used so the ssh doesn't require a password. If you are adapting this, you'll need to change pianobar.py to ssh into the machine you want (or, if your pi is connected to the speakers, just run pianobar).
+
+
+
+
+
+
--- ir_remote.py
+++ ir_remote.py
@@ -3,36 +3,126 @@ from fauxmo.fauxmo import poller,upnp_broadcast_responder,fauxmo
 import os
 import time
 
-# This handler class is used to control TV power button.
-# In this case, "on" and "off" do the same thing - transmit the 
-# power LED sequence
-class tv_power_handler(object):
-    def __init__(self):
+# Set a default remote name
+DEFAULT_REMOTE='SamsungBN59-01054A'
+
+# Base class for various TV handlers
+class tv_remote_handler(object):
+    def __init__(self,remote_name=DEFAULT_REMOTE):
+        """
+        Handler for controling TV with lirc
+
+        Parameters
+        ----------
+        remote_name:  Name of remote in /etc/lirc/lircd.conf file
+        """
         self.on_cmd = 'on'
         self.off_cmd = 'off'
+        self.remote_name=remote_name
+        self.key = 'KEY_POWER' # Set in superclass
+        self.cmd = 'irsend {s} {r} {k}'
+    
+    def issue(self,cmd):
+        """
+        Issues system command cmd
+        
+        Parameters
+        ----------
+             cmd  Command to run
+        """
+        os.system(cmd)
+   
+    def send(self,n=1):
+        """
+        Sends key one or more times
+
+        Parameters
+        ----------
+                 n  Number of times to send
+        """
+        for i in range(n):
+            self.issue(self.comd.format(s='SEND_ONCE',
+                                   r=self.remote_name,
+                                   k=self.key)
+            time.sleep(0.1)               
+
+    def send_continuous(self,send_length=0.1):
+        """
+        Sends IR signal continuously for length send_length seconds
+         
+        Parameters
+        ----------
+        send_length  Length of time that command is being sent
+
+        """
+        self.issue(self.cmd.format(s='SEND_START',
+                              r=self.remote_name,
+                              k=self.key))
+        time.sleep(send_length)
+        self.issue(self.cmd.format(s='SEND_STOP',
+                              r=self.remote_name,
+                              k=self.key)) 
+        
+   
+
+
+# This handler class is used to control TV power button.
+class tv_power_handler(tv_remote_handler):
+    def __init__(self,remote_name=DEFAULT_REMOTE):
+        """
+        Handler for tuning TV power on and off
+
+        Parameters
+        ----------
+        remote_name:  Name of remote in /etc/lirc/lircd.conf file
+
+        """
+        Base.__init__(self,remote_name=remote_name)
+        self.key='KEY_POWER'
 
     def on(self):
-        os.system('irsend SEND_START Samsung_BN59-00861A KEY_POWER')
-        time.sleep(0.1)
-        os.system('irsend SEND_STOP Samsung_BN59-00861A KEY_POWER')
+        """
+        Turns on TV
+
+        Note: I needed to send multiple "on" signals to my TV for 
+        this to work.
+        """
+        self.send_continuous(send_length(0.1))
         print "TV power on"
         return True
     def off(self):
-        os.system('irsend SEND_ONCE Samsung_BN59-00861A KEY_POWER')
+        """
+        Turns off TV
+        """
+        self.send(n=1)
         print "TV power off"
         return True
 
 class tv_mute_handler(object):
     def __init__(self):
-        self.on_cmd = 'on'
-        self.off_cmd = 'off'
+        """
+        Handler for tuning TV power on and off
+
+        Parameters
+        ----------
+        remote_name:  Name of remote in /etc/lirc/lircd.conf file
+
+        """
+        Base.__init__(self,remote_name=remote_name)
+        self.key='KEY_MUTE'
 
     def on(self):
-        os.system('irsend SEND_ONCE Samsung_BN59-00861A KEY_MUTE')
+        """
+        Turns on Mute
+        """
+        self.send(n=1)
         print "TV mute on"
         return True
     def off(self):
-        os.system('irsend SEND_ONCE Samsung_BN59-00861A KEY_MUTE')
+        """ 
+        Turns off mute
+        """
+        self.send(n=1)
         print "TV mute off"
         return True
 
